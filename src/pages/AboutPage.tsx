5dd65640c0a66fd23d5d9eb7946119e0be41110b
David <dsabalete@gmail.com>
2026-04-14 20:07:02 +0200

    feat: enhance AboutPage layout and content; improve styling for better readability

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -1,21 +1,31 @@
-import { useNavigate } from "react-router-dom";
-
 const AboutPage = () => {
-  const navigate = useNavigate();
-
-  const handleBack = () => {
-    navigate("/");
-  };
-
   return (
     <main className="about-page">
-      <div className="hero-section">
-        <h1>Sobre mi</h1>
-        <p>Soc jo.</p>
-        <button className="close-hero" onClick={handleBack}>
-          Enrere
-        </button>
-      </div>
+      <h1>👋 Hola! Soc Alex!</h1>
+      <p>
+        Des que vaig descobrir que la realitat es pot explicar millor a través
+        d'una lent, el cinema ha estat la meva brúixola. Com a estudiant de
+        <strong>Comunicació i Multimèdia</strong>, he après que una bona idea
+        necessita una execució tècnica impecable per transcendir.
+      </p>
+      <p>
+        No m'interessa només fer vídeos; vull crear{" "}
+        <strong>experiències</strong>. Crec que el sector audiovisual està en un
+        moment de canvi constant i vull ser part d'aquells que s'atreveixen a
+        innovar. El meu enfocament combina:{" "}
+      </p>
+
+      <ul>
+        <li>
+          <strong>Narrativa cinematogràfica</strong> clàssica.
+        </li>
+        <li>
+          <strong>Noves tecnologies</strong> multimèdia.
+        </li>
+        <li>
+          Una obsessió pel <strong>detall visual</strong> i l'impacte emocional.
+        </li>
+      </ul>
     </main>
   );
 };
